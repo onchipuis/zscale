54588a7d579e9722daa3da949eff23bd17c1c41d
ckdur <ckdur_black@hotmail.com>
2017-06-09 15:19:38 -0500

    Added DMI2Hasti

--- src/main/scala/zscaleuncore/devices/debug/Debug.scala
+++ src/main/scala/zscaleuncore/devices/debug/Debug.scala
@@ -8,6 +8,7 @@ import regmapper._
 import tile.XLen
 import config._
 import diplomacy._
+import HastiConstants._
 
 case object IncludeDMMem extends Field[Boolean]
 
@@ -21,6 +22,12 @@ class ZscaleDebugMemBundle(implicit p: Parameters) extends Bundle {
       )
   }))
 }
+
+/** From rocket-chip:
+  * Create a version of the ZscaleDebugModule which includes a synchronization interface
+  * internally for the DMI. This is no longer optional outside of this module
+  *  because the Clock must run when tlClock isn't running or tlReset is asserted.
+  */
 
 class ZscaleDebugModule(implicit p: Parameters) extends LazyModule {
 
@@ -63,5 +70,84 @@ class ZscaleDebugModule(implicit p: Parameters) extends LazyModule {
   }
   
   // TODO: Nothing yet
+}
+
+/** Convert DMI to AHB-Lite (Hasti). Avoids using special DMI synchronizers and register accesses
+  *  
+  */
+
+class DMIToHasti(implicit p: Parameters) extends LazyModule {
+
+  lazy val module = new LazyModuleImp(this) {
+    val io = new Bundle {
+      val dmi = new DMIIO()(p).flip()
+      val out = new HastiMasterIO()(p.alterPartial({
+        case HastiId => "debug"
+        case HastiKey("debug") => 
+          HastiParameters(
+            dataBits=p(XLen),
+            addrBits=p(XLen)
+          )
+      }))
+    }
+    
+    // CKDUR: Well, not into TileLink, but it works like a DecoupledIO, so we are going to
+    // just imitate this for now
+
+    val src  = 0.U
+    val addr = (io.dmi.req.bits.addr << 2)
+    val size = (log2Ceil(DMIConsts.dmiDataSize / 8)).U
+
+    val gbits_addr = addr
+    val pfbits_addr = addr
+    val pfbits_data = io.dmi.req.bits.data
+
+    // We force DMI NOPs to go to CONTROL register because
+    // Inner  may be in reset / not have a clock,
+    // so we force address to be the one that goes to Outer.
+    // Therefore for a NOP we don't really need to pay the penalty to go
+    // across the CDC.
+    
+    val nbits_addr = (DMI_RegAddrs.DMI_DMCONTROL << 2).U
+
+    when (io.dmi.req.bits.op === DMIConsts.dmi_OP_WRITE)       { io.out.haddr := pfbits_addr // Welp, address/data assign seems to be here
+                                                                 io.out.hwdata := pfbits_data
+                                                                 io.out.hwrite := Bool(true)
+                                                                 
+    }.elsewhen  (io.dmi.req.bits.op === DMIConsts.dmi_OP_READ) { io.out.haddr := gbits_addr 
+                                                                 io.out.hwdata := 0.U
+                                                                 io.out.hwrite := Bool(false)
+                                                                 
+    }.otherwise {                                                io.out.haddr := nbits_addr 
+                                                                 io.out.hwdata := 0.U
+                                                                 io.out.hwrite := Bool(false)
+    }
+    
+    // Captured data
+    val cap_data = Reg(init = UInt("h0", p(XLen)))
+    val valid = Reg(init = Bool(false))
+    when(io.out.hready & !io.dmi.resp.ready & !valid) // If there is data, but is not ready the DMI to receive it, then capture it and wait
+    {
+      cap_data := io.out.hrdata
+      valid := Bool(true)
+    }.elsewhen(valid & io.dmi.req.valid & io.dmi.resp.ready)
+    {
+      cap_data := 0.U
+      valid := Bool(false)
+    }
+    
+    io.out.hsize := size
+    io.out.hburst := HBURST_SINGLE
+    // TODO: This is ok? We are holding because the ready status for the response
+    io.out.htrans := Mux(io.dmi.req.valid & io.dmi.resp.ready, HTRANS_NONSEQ, HTRANS_IDLE)
+    
+    io.dmi.req.ready := io.out.hready
+
+    io.dmi.resp.valid      := io.out.hready       // TODO: This is ok?
+    io.dmi.resp.bits.resp  := DMIConsts.dmi_RESP_SUCCESS // TODO: Always success? If not, return DMIConsts.dmi_RESP_FAILURE
+    io.dmi.resp.bits.data  := Mux(valid, cap_data, io.out.hrdata) 
+    io.out.hprot := UInt("b0011")
+    io.out.hmastlock := Bool(false)
+  }
 }
 
